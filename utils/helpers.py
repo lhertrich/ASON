72103b65265787ca3cc2cdf174f1f448bcf729c8
Levin <st166200@stud.uni-stuttgart.de>
2025-10-01 22:56:27 +0200

    tried normalization without success

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from skimage.morphology import remove_small_objects, remove_small_holes, binary_closing, disk
 from skimage.transform import resize
 
@@ -17,3 +19,19 @@ def cut_out_image(image, mask):
     resized_mask = resized_mask.astype(bool)
     cut_image[~resized_mask] = 255
     return cut_image
+
+
+def compare_two_images(image_1, image_2, title_1 = None, title_2 = None, size=(12, 6)):
+    plt.figure(figsize=size)
+
+    plt.subplot(1, 2, 1)
+    plt.axis(False)
+    plt.title(title_1)
+    plt.imshow(image_1)
+
+    plt.subplot(1, 2, 2)
+    plt.axis(False)
+    plt.title(title_2)
+    plt.imshow(image_2)
+
+    plt.show()
